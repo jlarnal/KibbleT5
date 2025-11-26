49a18a9d1fc72eb9c9c8719bb44f350d0b4d296a
jlarnal <arnaljl@gmail.com>
2025-11-26 02:08:04 +0100

    Changed some misleading comments in `board_pinout.h`

--- include/board_pinout.h
+++ include/board_pinout.h
@@ -12,10 +12,11 @@
 
 #define BATT_HALFV_PIN (35)
 
-
+// Apart from the custom "power" pin, the PCA9685 is controlled using the default I2C pinout.
 #define SERVO_POWER_ENABLE_PIN (33)
 
-// The original ONE_WIRE_BUS is now multiplexed through a 74HC4051 (analog multiplexer)
+// The 1kbits 1-wire eeprom are multiplexed through a CH32V003 slave MCU named "SwiMux" and
+// accessed through the SwiMuxSerial adapter class, using one UART peripheral.
 // This allows for up to 6 tanks, each on its own bus for identification.
 #define SWIMUX_SERIAL_DEVICE Serial2
 #define SWIMUX_TX_PIN (27)
